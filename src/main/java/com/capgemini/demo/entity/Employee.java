7576cfb3282c4625e1b7fc52919c8e1a90737e69
Sreeja <thagirancha.sreeja@capgemini.com>
2026-04-08 15:07:10 +0530

    Fix entity configuration, update app config, and stabilize application startup

--- src/main/java/com/capgemini/demo/entity/Employee.java
+++ src/main/java/com/capgemini/demo/entity/Employee.java
@@ -1,42 +1,43 @@
 package com.capgemini.demo.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "employee")
 public class Employee {
-	
-	@Id
-	private int empId;
-	private String empName;
-	private double salary;
-	
-	public Employee() {}
-	
-	public Employee(int empId, String empName,double salary) {
-		this.empId=empId;
-		this.empName=empName;
-		this.salary=salary;
-	}
-	
-	public int getEmpId() {
-		return empId;
-		
-	}
-	public void setEmpId(int empId) {
-		this.empId = empId;
-	}
-	public String getEmpName() {
-		return empName;
-	}
-	
-	public void setEmpName(String empName) {
-		this.empName=empName;
-	}
-	
-	public double getSalary() {
-		return salary;
-	}
-	public void setSalary(double salary) {
-		this.salary=salary;
-	}
 
+    @Id
+    private int empId;
+
+    private String empName;
+    private double salary;
+
+    public Employee() {
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
